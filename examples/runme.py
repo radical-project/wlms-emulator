71fa8a6482243676072726b2d4e01357aafbcd1c
krb09 <kartikrattan09@gmail.com>
2020-03-24 11:26:11 -0500

    try

--- examples/runme.py
+++ examples/runme.py
@@ -8,7 +8,7 @@ def get_workload():
 
     # Create a workload with a specific number of tasks with number of
     # operations per task drawn from a distribution
-    wl = Workload(  num_tasks=128,          # no. of tasks
+    wl = Workload(  num_tasks=5,          # no. of tasks
                     ops_dist='uniform',     # distribution to draw samples from
                     dist_mean=1024,         # mean of distribution
                     dist_var=4              # variance of distribution
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
     # Create a resource with a specific number of cores with performance of each
     # core drawn from a distribution
-    res = Resource( num_cores=128,        # no.
+    res = Resource( num_cores=5,        # no.
                     perf_dist='uniform',# distribution to draw samples from
                     dist_mean=32,        # mean of distribution
                     temporal_var=1,     # temporal variance of core performance
